9facbda59f9d1581afbfac26c91ce6baba7ca1f3
janvanrijn <janvanrijn@gmail.com>
2019-01-15 16:02:04 -0500

    added proper output

--- src/org/openml/tools/dataset/DeactivateUnparsableDatasets.java
+++ src/org/openml/tools/dataset/DeactivateUnparsableDatasets.java
@@ -1,6 +1,8 @@
 package org.openml.tools.dataset;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +26,7 @@ public class DeactivateUnparsableDatasets {
 	private static final String test_key_write = "8baa83ecddfe44b561fd3d92442e3319";
 	private static final String live_key_read = "c1994bdb7ecb3c6f3c8f3b35f4b47f1f"; // mlr ..  sorry i borrowed it
 	private static final XStream xstream = XstreamXmlMapping.getInstance();
+	private static final String output_filepath = "unparsable.bout";
 	
 	private static final OpenmlConnector test_client_write = new OpenmlConnector(test_url, test_key_write);
 	private static final OpenmlConnector live_client_read = new OpenmlConnector(live_url, live_key_read);
@@ -41,7 +44,8 @@ public class DeactivateUnparsableDatasets {
 				continue;
 			}
 			DataSetDescription dsd = live_client_read.dataGet(current.getDid());
-			File dsdFile = Conversion.stringToTempFile(xstream.toXML(dsd), "did-" + current.getDid(), "arff");
+			DataSetDescription dsdNew = new DataSetDescription(dsd.getName(), dsd.getDefault_target_attribute(), dsd.getFormat(), dsd.getDefault_target_attribute());
+			File dsdFile = Conversion.stringToTempFile(xstream.toXML(dsdNew), "did-" + current.getDid(), "arff");
 			File dataset = live_client_read.datasetGet(dsd);
 			
 			
@@ -59,5 +63,10 @@ public class DeactivateUnparsableDatasets {
 				Conversion.log("Warning", "Upload", "Problem uploading dataset " + current.getDid() + ": " + e.getMessage());
 			}
 		}
+		
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(output_filepath));
+		oos.writeObject(unparsable);
+		oos.flush();
+		oos.close();
 	}
 }
