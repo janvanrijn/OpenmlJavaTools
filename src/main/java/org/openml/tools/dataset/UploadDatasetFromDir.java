d0fceb7bb3c402010e4381c3e9e3551ea3f1413a
janvanrijn <janvanrijn@gmail.com>
2019-05-31 02:10:20 +0200

    fixes upload script

--- src/main/java/org/openml/tools/dataset/UploadDatasetFromDir.java
+++ src/main/java/org/openml/tools/dataset/UploadDatasetFromDir.java
@@ -17,12 +17,11 @@ public class UploadDatasetFromDir {
 	private static final String APIKEY = "d488d8afd93b32331cf6ea9d7003d4c3";
 	private static final OpenmlWekaConnector openml = new OpenmlWekaConnector(SERVER, APIKEY);
 	
-	private static final String[] TAGS = {"forex", "finance"};
 	private static final File DIRECTORY = new File("/Users/janvanrijn/data/arff_forex");
 	private static final String DESCRIPTION = "**Source**: Dukascopy Historical Data Feed https://www.dukascopy.com/swiss/english/marketwatch/historical/\n" + 
 			"**Edited by**: Fabian Schut\n" + 
 			" \n" + 
-			"= Data Description\n" +
+			"# Data Description\n" +
 			"This is the historical price data of the FOREX $pair from Dukascopy.\n" + 
 			"One instance (row) is one candlestick of one $interval.\n" + 
 			"The whole dataset has the data range from 1-1-2018 to 13-12-2018 and does not include the weekends, since the FOREX is not traded in the weekend.\n" + 
@@ -30,9 +29,9 @@ public class UploadDatasetFromDir {
 			"The class attribute is the direction of the mean of the $type_Bid and the $type_Ask of the following $interval,\n" + 
 			"relative to the $type_Bid and $type_Ask mean of the current minute.\n" + 
 			"This means the class attribute is True when the mean $type price is going up the following $interval,\n" + 
-			"and the class attribute is False when the mean $type price is going down the following $interval." + 
+			"and the class attribute is False when the mean $type price is going down the following $interval.\n" + 
 			"$note" +
-			"= Attributes \n" +
+			"# Attributes \n" +
 			"`Timestamp`: The time of the current data point (Europe/Amsterdam)\n" +
 			"`Bid_Open`: The bid price at the start of this time interval\n" +
 			"`Bid_High`: The highest bid price during this time interval\n" +
@@ -49,17 +48,20 @@ public class UploadDatasetFromDir {
 	public static void main(String[] args) throws Exception {
 		List<Integer> taskIds = new ArrayList<Integer>(); 
 		for (File dataset : DIRECTORY.listFiles()) {
+			
 			String nameVanilla = dataset.getName().split("\\.")[0];
 			String[] nameParts = nameVanilla.split("_")[1].split("-");
 			String pair = nameParts[0].substring(0, 3) + '/' + nameParts[0].substring(3);
-			String note = nameParts[0] == "Close" ? "" : "**Note that this is a hypothetical task, ment for scientific purposes only. Realistic trade strategies can only be applied to predictions on 'Close'-attributes (also available)."; 
+			String note = nameParts[2].contentEquals("Close") ? "" : "**Note that this is a hypothetical task, ment for scientific purposes only. Realistic trade strategies can only be applied to predictions on 'Close'-attributes (also available).\n"; 
 			String description = DESCRIPTION.replace("$pair", pair.toUpperCase()).replace("$interval", nameParts[1]).replace("$type", nameParts[2]).replace("$note", note);
 			
 			DataSetDescription dsd = new DataSetDescription(nameVanilla, description, "arff", "Class");
-			int did = openml.dataUpload(dsd, dataset);
+			final String[] TAGS = {"finance", "forex", "forex_" + nameParts[1].toLowerCase(), "forex_" + nameParts[2].toLowerCase()};
 			for (String tag : TAGS) {
-				openml.dataTag(did, tag);
+				dsd.addTag(tag);
 			}
+			int did = openml.dataUpload(dsd, dataset);
+			
 			// automatically processes and activates dataset
 			new ProcessDataset(openml, did, null);
 			Input[] inputs = {
